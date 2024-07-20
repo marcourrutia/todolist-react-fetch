e174dcd09f9ea293e9d16b599ce0f575c1e84cb1
marcourrutia <m.urrutia1991@gmail.com>
2024-07-19 21:33:59 -0400

    probando fetch

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,11 +1,27 @@
 import "./ToDoList.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const ToDoList = () => {
   const [inputText, setInputText] = useState("");
   const [list, setList] = useState([]);
 
-  const handleOnChange = (event) => {
+  const getList = () => {
+    fetch("https://playground.4geeks.com/todo/users/devMarco", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setList(data.todos))
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    getList();
+  }, []);
+  console.log(list);
+
+  /* const handleOnChange = (event) => {
     setInputText(event.target.value);
   };
   const handleOnKeyDown = (event) => {
@@ -16,9 +32,9 @@ export const ToDoList = () => {
   };
   const removeItem = (indice) => {
     setList((prevList) => prevList.filter((_, i) => i !== indice));
-  };
+  }; */
 
-  return (
+  /* return (
     <>
       <h1>MIS TAREAS</h1>
       <div className="container">
@@ -41,5 +57,5 @@ export const ToDoList = () => {
         <p>Tareas pendientes: {list.length}</p>
       </div>
     </>
-  );
+  ); */
 };
